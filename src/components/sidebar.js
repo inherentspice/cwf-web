8a8c37d89ed2cb3433f8f4aeeb2a00a74ea0aaa6
inherentspice <stephenlehanesmith@gmail.com>
2023-03-13 14:10:07 +0000

    created login form in sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,56 @@
-import React from "react";
-import { Button } from "@mui/material";
+import React, { useState } from "react";
+import { Button, TextField, InputAdornment, Box } from "@mui/material";
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+
 
 export default function SideBar() {
+
+  const [ username, setUsername ] = useState("");
+  const [ password, setPassword ] = useState("");
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log(username, password);
+  }
+
+
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
-      <Button variant="contained" color="primary">Let's Button</Button>
+      <form onSubmit={handleSubmit}>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
+          <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
+          <TextField
+            id="input-with-sx"
+            label="Username"
+            variant="standard"
+            sx={{
+              "& .MuiInputLabel-root": {
+                color: "white",
+                opacity: .9,
+              }
+            }}
+            onChange={ e => setUsername(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
+          <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
+          <TextField
+            id="input-with-sx"
+            label="Password"
+            variant="standard"
+            sx={{
+              "& .MuiInputLabel-root": {
+                color: "white",
+                opacity: .9,
+              }
+            }}
+            type="password"
+            onChange={ e => setPassword(e.target.value)}
+          />
+        </Box>
+        <Button variant="contained" color="primary" type="submit">Login</Button>
+      </form>
     </div>
   )
 }
