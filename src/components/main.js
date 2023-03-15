f576a4be842e3037f117a42ca0a88bb307fc3085
inherentspice <stephenlehanesmith@gmail.com>
2023-03-15 13:15:18 +0000

    created logout button and functionality

--- src/components/main.js
+++ src/components/main.js
@@ -2,10 +2,15 @@ import React from "react";
 import GroupList from "./group-list"
 import GroupDetails from "./group-details";
 import { Routes, Route } from "react-router-dom";
+import { useAuth } from "../hooks/use-auth";
 
 export default function Main() {
+
+  const { authData } = useAuth();
+
   return (
     <div className="main">
+      {authData && <h3>{authData.user.username}</h3>}
       <Routes>
         <Route exact path="/" element={<GroupList/>}/>
         <Route path="/details/:id" element={<GroupDetails/>}/>
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -16,47 +16,56 @@ export default function SideBar() {
     e.preventDefault();
     const authDataJson = await auth({username, password});
     setAuth(authDataJson);
-    //setAuthData(authDataJson)
+  }
+
+  const logout = () => {
+    setAuth(null);
   }
 
 
   return (
     <div className="sidebar">
-      {authData && <p>{authData.token}</p>}
-      <form onSubmit={handleSubmit}>
-        <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
-          <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
-          <TextField
-            id="input-with-sx"
-            label="Username"
-            variant="standard"
-            sx={{
-              "& .MuiInputLabel-root": {
-                color: "white",
-                opacity: .9,
-              }
-            }}
-            onChange={ e => setUsername(e.target.value)}
-          />
-        </Box>
-        <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
-          <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
-          <TextField
-            id="input-with-sx"
-            label="Password"
-            variant="standard"
-            sx={{
-              "& .MuiInputLabel-root": {
-                color: "white",
-                opacity: .9,
-              }
-            }}
-            type="password"
-            onChange={ e => setPassword(e.target.value)}
-          />
-        </Box>
-        <Button variant="contained" color="primary" type="submit">Login</Button>
-      </form>
+      {!authData ?
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
+            <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
+            <TextField
+              id="input-with-sx"
+              label="Username"
+              variant="standard"
+              sx={{
+                "& .MuiInputLabel-root": {
+                  color: "white",
+                  opacity: .9,
+                }
+              }}
+              onChange={ e => setUsername(e.target.value)}
+            />
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
+            <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
+            <TextField
+              id="input-with-sx"
+              label="Password"
+              variant="standard"
+              sx={{
+                "& .MuiInputLabel-root": {
+                  color: "white",
+                  opacity: .9,
+                }
+              }}
+              type="password"
+              onChange={ e => setPassword(e.target.value)}
+            />
+          </Box>
+          <Button variant="contained" color="primary" type="submit">Login</Button>
+        </form>
+        :
+        <div>
+          <p>{authData.user.username}</p>
+          <Button variant="contained" color="primary" onClick={() => logout()}>Logout</Button>
+        </div>
+      }
     </div>
   )
 }
