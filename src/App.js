da25a40f8ff1cf22ae2a840d678f9c5678973915
inherentspice <stephenlehanesmith@gmail.com>
2023-03-15 13:05:49 +0000

    set-up useAuth hook to work with local storage method

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ import Main from "./components/main";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./hooks/use-auth";
 
 function App() {
 
+  const user = JSON.parse(localStorage.getItem("cwf-user"));
+
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Router>
-          <Header/>
-          <div className="general-content">
-            <SideBar/>
-            <Main/>
-          </div>
-        </Router>
-      </div>
+      <AuthProvider user={user}>
+        <div className="App">
+          <Router>
+            <Header/>
+            <div className="general-content">
+              <SideBar/>
+              <Main/>
+            </div>
+          </Router>
+        </div>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,22 +3,26 @@ import { Button, TextField, InputAdornment, Box } from "@mui/material";
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import LockIcon from '@mui/icons-material/Lock';
 import { auth } from "../services/user-services";
+import { useAuth } from "../hooks/use-auth";
 
 
 export default function SideBar() {
 
   const [ username, setUsername ] = useState("");
   const [ password, setPassword ] = useState("");
+  const { authData, setAuth } = useAuth();
 
   const handleSubmit = async e => {
     e.preventDefault();
-    const authData = await auth({username, password});
-    console.log(authData);
+    const authDataJson = await auth({username, password});
+    setAuth(authDataJson);
+    //setAuthData(authDataJson)
   }
 
 
   return (
     <div className="sidebar">
+      {authData && <p>{authData.token}</p>}
       <form onSubmit={handleSubmit}>
         <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
           <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
