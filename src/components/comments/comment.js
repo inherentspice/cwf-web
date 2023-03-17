fe8202208cc1be0d1151bff23706bc66bb79648f
inherentspice <stephenlehanesmith@gmail.com>
2023-03-17 11:13:38 +0000

    styled comments and added time published

--- src/components/comments/comment.js
+++ src/components/comments/comment.js
@@ -1,11 +1,22 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import User from "../user/user";
 
 export default function Comment({comment, user}) {
   return (
-    <div>
+    <div className="comment-cont">
       <User user={user}/>
-      <p>{comment.description}</p>
+      <div className="dialog-box">
+        <div className="dialog-body">
+          <span className="tip">&nbsp;</span>
+          <div className="body-message">
+            <span>{comment.description}</span>
+          </div>
+        </div>
+        <Typography variant="p" className="time">
+          {comment.time.split('T')[0]} &nbsp;{comment.time.split('T')[1].substring(0, 5)}
+        </Typography>
+      </div>
     </div>
   )
 }
