5dfe8cac5e76eba0c45d384032210d97342a9fe6
inherentspice <stephenlehanesmith@gmail.com>
2023-03-18 14:45:40 +0000

    fixed display of dates, used conditional rendering for making bets

--- src/components/events/event.js
+++ src/components/events/event.js
@@ -19,14 +19,23 @@ export default function Event({}) {
   const { authData } = useAuth();
   const [ data, loading, error] = useFetchEvent(authData.token, id);
   const [ event, setEvent ] = useState(null);
-  const [ eventTime, setEventTime ] = useState(null);
+  const [ eventStartTime, setEventStartTime ] = useState(null);
+  const [ eventEndTime, setEventEndTime ] = useState(null);
+  const [ isFuture, setIsFuture ] = useState(false);
+  const [ timeDifference, setTimeDifference ] = useState(null);
   const [ pricePrediction, setPricePrediction ] = useState(null);
 
   useEffect(() => {
     setEvent(data);
     if (data?.time) {
       const format = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-      setEventTime(DateTime.fromFormat(data.time, format));
+      const startTime = DateTime.fromFormat(data.time, format)
+      const endTime = DateTime.fromFormat(data.end_time, format)
+      setEventStartTime(startTime);
+      setEventEndTime(endTime)
+      const now = DateTime.now();
+      setIsFuture(endTime > now);
+      setTimeDifference(endTime.toRelative())
     }
   }, [data]);
 
@@ -51,17 +60,22 @@ export default function Event({}) {
 
   return (
     <>
-      {event && eventTime &&
+      {event && eventStartTime &&
         <div>
           <Link to={`/details/${event.group}`}><ArrowBackIcon/></Link>
           <h2>{event.crypto}</h2>
           <p>Starting Price: ${event.price_start}</p>
           <p>Current Price: </p>
           <p>Ending Price: {event.price_end ? `$${event.price_end}` : ""}</p>
-          <h3>
-            <CalendarTodayIcon className="dateTime"/>{eventTime.toSQLDate()}
-            <AccessTimeIcon className="dateTime"/>{eventTime.toFormat("HH:mm")}
+          <h3> Start Time:
+            <CalendarTodayIcon className="dateTime"/>{eventStartTime.toSQLDate()}
+            <AccessTimeIcon className="dateTime"/>{eventStartTime.toFormat("HH:mm")}
           </h3>
+          <h3> End Time:
+            <CalendarTodayIcon className="dateTime"/>{eventEndTime.toSQLDate()}
+            <AccessTimeIcon className="dateTime"/>{eventEndTime.toFormat("HH:mm")}
+          </h3>
+          <h3>{timeDifference}</h3>
           <hr/>
           { event && event.bets && event.bets.map(bet => {
             return <div key={bet.id} className="bets">
@@ -70,18 +84,23 @@ export default function Event({}) {
               <h4>Points: </h4>
             </div>
           })}
-          <hr/>
-          <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
-            <CurrencyBitcoinIcon sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
-            <TextField
-              label="Price Prediction"
-              variant="standard"
-              sx={textFieldStyling}
-              type="number"
-              onChange={ e => setPricePrediction(e.target.value)}
-            />
-          </Box>
-          <Button variant="contained" color="secondary" onClick={() => sendBet()} disabled={!pricePrediction}>Predict</Button>
+          { isFuture &&
+            <div>
+              <hr/>
+
+              <Box sx={{ display: 'flex', alignItems: 'flex-end'}}>
+                <CurrencyBitcoinIcon sx={{ color: 'action.active', mr: 1, my: 0.5, color: 'white'}} />
+                <TextField
+                  label="Price Prediction"
+                  variant="standard"
+                  sx={textFieldStyling}
+                  type="number"
+                  onChange={ e => setPricePrediction(e.target.value)}
+                />
+              </Box>
+              <Button variant="contained" onClick={() => sendBet()} color="secondary" disabled={!pricePrediction}>Place Bet</Button>
+            </div>
+            }
         </div>
       }
     </>
