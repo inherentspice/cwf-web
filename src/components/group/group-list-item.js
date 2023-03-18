9756281f566ec24cdcdff9b713f209b8e84d22c0
inherentspice <stephenlehanesmith@gmail.com>
2023-03-18 19:31:37 +0000

    separated individual groups into own component

---
+++ src/components/group/group-list-item.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import GroupIcon from '@mui/icons-material/Group';
+
+export default function GroupListItem({group}) {
+
+  return (
+    <div className="group-list-item-cont">
+
+      <Link to={`/details/${group.id}`}>
+        <h3><span className="highlight">{group.name}:</span> {group.location}</h3>
+      </Link>
+
+
+      <div className="member-count-cont">
+        <GroupIcon/> {group.num_members}
+      </div>
+    </div>
+  );
+}
