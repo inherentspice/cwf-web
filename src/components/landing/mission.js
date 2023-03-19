71d6242c6bc545d64e7795e7f6a02cfe51dff4d5
inherentspice <stephenlehanesmith@gmail.com>
2023-03-19 12:58:59 +0000

    finished basic layout and functionality of mission page

--- src/components/landing/mission.js
+++ src/components/landing/mission.js
@@ -1,5 +1,37 @@
-export default function Mission() {
+import BackgroundImage from "../../assets/mission-image.png"
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function Landing() {
+  const navigate = useNavigate();
+
+  const groupClick = () => {
+    navigate("/");
+  }
+
   return (
-    <h1>Mission</h1>
+    <div className="mission-cont">
+      <div className="landing-info-cont">
+        <h1>Re-Imagining Web3 Engagement</h1>
+        <p>
+          At CryptoPredict, our mission is to empower individuals through a unique combination of fun, education, and an immersive glimpse into the future of finance.
+          We strive to create a platform that sparks curiosity and fosters continuous learning while providing an entertaining and engaging environment.
+          Our vision is to become the go-to destination for crypto enthusiasts, gamblers, and novices alike, as they embark on a journey to uncover the immense potential of digital currencies and the underlying blockchain technology.
+        </p>
+        <p>
+          In the pursuit of this mission, we are committed to cultivating a strong community spirit that encourages collaboration, friendly competition, and knowledge sharing.
+          We believe that by fostering meaningful connections and providing opportunities for users to learn from each other, we can elevate the collective understanding of the complex and rapidly evolving world of cryptocurrencies.
+          Our platform is designed to be accessible and enjoyable for everyone, regardless of their background or experience level, ensuring that the doors of the future are wide open for all.
+        </p>
+        <p>
+          As we look towards the future, CryptoPredict will continue to innovate and adapt to the ever-changing landscape of digital finance.
+          We will actively seek out new opportunities to expand our offerings, integrate emerging technologies, and create even more engaging experiences for our users.
+          With a steadfast dedication to fun, education, and the future, we aim to inspire our users to embrace the exciting possibilities that lie ahead with confidence and enthusiasm.
+          Together, we will navigate the uncharted waters of the crypto economy and shape a more connected, informed, and prosperous tomorrow.
+        </p>
+        <Button variant="contained" color="secondary" onClick={() => groupClick()}>Play</Button>
+      </div>
+      <img src={BackgroundImage} alt="Background image" className="mission-background"/>
+    </div>
   )
 }
